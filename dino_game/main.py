4c57e844e1546ad55decc701f2b591716dbdc517
Umepejo <71135988+Umepejo@users.noreply.github.com>
2021-11-08 11:07:01 +0100

    bored

--- dino_game/main.py
+++ dino_game/main.py
@@ -1,26 +1,34 @@
 import os
 import pygame as pg
 from pygame import image
-from settings import Settings
+from pygame.constants import K_LSHIFT, K_SPACE
+from settings import Settings as S
  
 pg.init()
+cwd = os.getcwd()
+
 
 #Constants
 run = True
 
 #Dino posistions and actions
+d_width = 20
+d_height = 20
 direction = ""
 jump = False
 last_dir = "right"
 m_grounded = True
+m_run = False
 xpos = 50
-ypos = 100
+ypos = S.dis_height / 2
+dinoRect = pg.Rect(ypos + 10, xpos + 10, d_width, d_height)
 
 #Lists
 all_animation_lists = []
 d_idle = []
 d_walk = []
 d_jump = []
+d_run = []
 char = []
 jump_return = []
 
@@ -31,9 +39,35 @@ leftOrRight = False
 delay = False
 falling_modifier = 1
 
+bg_img = pg.image.load(cwd+'/pygame/dino_game/assets/1-1.png')
+
 #Setting up display
-dis = pg.display.set_mode((Settings.dis_width, Settings.dis_height))
-dis.fill(Settings.background)
+dis = pg.display.set_mode((S.dis_width, S.dis_height))
+dis.blit(bg_img, (0, 0))
+
+def _collisions_setup():
+    
+    groundRect = 0
+    setup_list = []
+    rectList = [(0,208,1103,208),(1136,208,239,208),(1424,208,1023,208),(2480,208,895,208)]
+
+    for x in rectList:
+        groundRect = pg.Rect(x)
+        setup_list.append(groundRect)
+    
+    return setup_list
+
+m_ground = _collisions_setup()
+
+def _check_collisions(char_rect):
+    ground = m_ground
+    temp = False
+    for x in ground:
+        if pg.Rect.colliderect(x, char_rect):
+            temp = True
+
+    return temp
+            
 
 def _animation_setup(): #Stuff that should be ran once but looks better separated from main body of code (maybe diffrent file?)
     def _image_at(sheet, number, width, height, scale, flip): #Ran multiple times but all times at the same time
@@ -47,40 +81,58 @@ def _animation_setup(): #Stuff that should be ran once but looks better separate
         image = pg.transform.scale(image, (width * scale, height * scale))
         image.set_colorkey((0, 0, 0))
         return image
-    
-    cwd = os.getcwd()
 
-    dino_sheet = pg.image.load(os.path.normpath(cwd+'/dino_game/assets/DinoSprites.png')).convert_alpha()
+    dino_sheet = pg.image.load(os.path.normpath(cwd+'/pygame/dino_game/assets/DinoSprites.png')).convert_alpha()
     
     d_idle_list = []
     d_walk_list = []
     d_jump_list = []
+    d_run_list = []
+    d_size_modif = 1.5
 
     #Assign sprites
-    d_idle_list.append(_image_at(dino_sheet, 0, 24, 24, 3, False))
-    d_idle_list.append(_image_at(dino_sheet, 0, 24, 24, 3, True))
-    d_walk_list.append(_image_at(dino_sheet, 5, 24, 24, 3, False))
-    d_walk_list.append(_image_at(dino_sheet, 6, 24, 24, 3, False))
-    d_walk_list.append(_image_at(dino_sheet, 7, 24, 24, 3, False))
-    d_walk_list.append(_image_at(dino_sheet, 8, 24, 24, 3, False))
-    d_walk_list.append(_image_at(dino_sheet, 9, 24, 24, 3, False))
-    d_jump_list.append(_image_at(dino_sheet, 11, 24, 24, 3, False))
-    d_jump_list.append(_image_at(dino_sheet, 12, 24, 24, 3, False))
-    d_jump_list.append(_image_at(dino_sheet, 11, 24, 24, 3, True))
-    d_jump_list.append(_image_at(dino_sheet, 12, 24, 24, 3, True))
+    d_idle_list.append(_image_at(dino_sheet, 0, 24, 24, d_size_modif, False))
+    d_idle_list.append(_image_at(dino_sheet, 0, 24, 24, d_size_modif, True))
+    d_walk_list.append(_image_at(dino_sheet, 5, 24, 24, d_size_modif, False))
+    d_walk_list.append(_image_at(dino_sheet, 6, 24, 24, d_size_modif, False))
+    d_walk_list.append(_image_at(dino_sheet, 7, 24, 24, d_size_modif, False))
+    d_walk_list.append(_image_at(dino_sheet, 8, 24, 24, d_size_modif, False))
+    d_walk_list.append(_image_at(dino_sheet, 9, 24, 24, d_size_modif, False))
+    d_jump_list.append(_image_at(dino_sheet, 11, 24, 24, d_size_modif, False))
+    d_jump_list.append(_image_at(dino_sheet, 12, 24, 24, d_size_modif, False))
+    d_jump_list.append(_image_at(dino_sheet, 11, 24, 24, d_size_modif, True))
+    d_jump_list.append(_image_at(dino_sheet, 12, 24, 24, d_size_modif, True))
+    d_run_list.append(_image_at(dino_sheet, 17, 24, 24, d_size_modif, False))
+    d_run_list.append(_image_at(dino_sheet, 18, 24, 24, d_size_modif, False))
+    d_run_list.append(_image_at(dino_sheet, 19, 24, 24, d_size_modif, False))
+    d_run_list.append(_image_at(dino_sheet, 20, 24, 24, d_size_modif, False))
+    d_run_list.append(_image_at(dino_sheet, 21, 24, 24, d_size_modif, False))
+    d_run_list.append(_image_at(dino_sheet, 22, 24, 24, d_size_modif, False))
+    d_run_list.append(_image_at(dino_sheet, 23, 24, 24, d_size_modif, False))
+    d_run_list.append(_image_at(dino_sheet, 17, 24, 24, d_size_modif, True))
+    d_run_list.append(_image_at(dino_sheet, 18, 24, 24, d_size_modif, True))
+    d_run_list.append(_image_at(dino_sheet, 19, 24, 24, d_size_modif, True))
+    d_run_list.append(_image_at(dino_sheet, 20, 24, 24, d_size_modif, True))
+    d_run_list.append(_image_at(dino_sheet, 21, 24, 24, d_size_modif, True))
+    d_run_list.append(_image_at(dino_sheet, 22, 24, 24, d_size_modif, True))
+    d_run_list.append(_image_at(dino_sheet, 23, 24, 24, d_size_modif, True))
 
-    return d_idle_list, d_walk_list, d_jump_list
+    return d_idle_list, d_walk_list, d_jump_list, d_run_list
     
 all_animation_lists = _animation_setup()
 d_idle = all_animation_lists[0]
 d_walk = all_animation_lists[1]
 d_jump = all_animation_lists[2]
+d_run = all_animation_lists[3]
 
-def _move_char(flip, x, y, anim_num): #Simple move script
+def _move_char(flip, x, y, anim_num, dino_run):
     #Define
     change = 0
-    speed = 10
+    speed = 8
     char_img = ""
+
+    if dino_run:
+        speed = 12
 
     #Moving left or right based on input + cycling animation
     anim_num += 1
@@ -91,19 +143,25 @@ def _move_char(flip, x, y, anim_num): #Simple move script
     change = speed
     x += change
 
-    char_img = pg.transform.flip(d_walk[anim_num], flip, False)
+    if not dino_run:
+        char_img = pg.transform.flip(d_walk[anim_num], flip, False)
+    else:
+        char_img = pg.transform.flip(d_run[anim_num], flip, False)
     char_img.set_colorkey((0, 0, 0))
     char_temp = [char_img, x, y, anim_num]
 
     return char_temp
 
-def _jump(dire, grounded, x, y, anim_list):
+def _jump(dire, grounded, x, y, anim_list, dino_run, dino_rect):
     #Define
-    speed = 10
+    speed = 8
     change = 0
     jump_up = -15
     anim_num_j = -1
     anim_change = 0
+
+    if dino_run:
+        speed = 12
 
     if dire == "left":
         speed = -speed
@@ -123,13 +181,19 @@ def _jump(dire, grounded, x, y, anim_list):
             anim_num_j = 1 + anim_change
         jump_up += 1
         x += change
-        dis.fill(Settings.background)
+        dis.blit(bg_img, (0, 0))
         dis.blit(anim_list[anim_num_j], (x, y))
+        dinoRect = (x + 10, y + 10, d_width, d_height)
+        grounded = _check_collisions(dino_rect)
+        pg.draw.rect(dis, (0,0,255), (0,208,1103,208))
+        pg.draw.rect(dis, (255,0,0), dinoRect)
         pg.display.flip()    
         pg.time.delay(50)
-        if y >= 100:
-            y = 100
-            grounded = True
+        dino_rect = (x, y, d_width, d_height)
+        grounded = _check_collisions(dino_rect)
+        #if y >= S.dis_height / 2:
+            #y = S.dis_height / 2
+            #grounded = True
         keys = pg.key.get_pressed()
     pg.time.delay(10)
     if keys[pg.K_RIGHT]:
@@ -159,44 +223,54 @@ while run:
                 jump = True
                 delay = True
 
-    pg.time.delay(100)
-
     if direction == "right":
         if jump:
-            jump_return = _jump(last_dir, m_grounded, xpos, ypos, d_jump)
+            if keys[K_LSHIFT]:
+                m_run = True
+            jump_return = _jump(last_dir, m_grounded, xpos, ypos, d_jump, m_run, dinoRect)
             action = ""
             xpos = jump_return[0]
             ypos = jump_return[1]
             last_dir = jump_return[2]
             jump = False
+            m_run = False
         else:
-            char = _move_char(False, xpos, ypos, a_num)
+            if keys[K_LSHIFT]:
+                m_run = True
+            char = _move_char(False, xpos, ypos, a_num, m_run)
             last_dir = "right"
             a_num = -1
             leftOrRight = True
             xpos = char[1]
             ypos = char [2]
             a_num = char[3]
-            dis.fill(Settings.background)
+            dis.blit(bg_img, (0, 0))
             dis.blit(char[0], (xpos, ypos))
+            m_run = False
     elif direction == "left":
         if jump:
-            jump_return = _jump(last_dir, m_grounded, xpos, ypos, d_jump)
+            if keys[K_LSHIFT]:
+                m_run = True
+            jump_return = _jump(last_dir, m_grounded, xpos, ypos, d_jump, m_run, dinoRect)
             action = ""
             xpos = jump_return[0]
             ypos = jump_return[1]
             last_dir = jump_return[2]
             jump = False
+            m_run = False
         else:
-            char = _move_char(True, xpos, ypos, a_num)
+            if keys[K_LSHIFT]:
+                m_run = True
+            char = _move_char(True, xpos, ypos, a_num, m_run)
             last_dir = "left"
             a_num = -1
             leftOrRight = True
             xpos = char[1]
             ypos = char [2]
             a_num = char[3]
-            dis.fill(Settings.background)
+            dis.blit(bg_img, (0, 0))
             dis.blit(char[0], (xpos, ypos))
+            m_run = False
     
     if not leftOrRight and jump:
         for x in range(-15, 0, 1):
@@ -205,7 +279,7 @@ while run:
                 a_num = 2
             else:
                 a_num = 0
-            dis.fill(Settings.background)
+            dis.blit(bg_img, (0, 0))
             dis.blit(d_jump[a_num], (xpos, ypos))
             pg.display.flip()
             pg.time.delay(50)
@@ -217,27 +291,23 @@ while run:
             else:
                 a_num = 1
             falling_modifier += 1
-            dis.fill(Settings.background)
+            dis.blit(bg_img, (0, 0))
             dis.blit(d_jump[a_num], (xpos, ypos))
+            pg.draw.rect(dis, (0,0,255), (0,208,1103,208))
+            pg.draw.rect(dis, (255,0,0), dinoRect)
             pg.display.flip()
             pg.time.delay(50)
-            if ypos > 100:
-                m_grounded = True
+            m_grounded = _check_collisions((dinoRect))
         falling_modifier = 1
         jump = False
 
 
     if not leftOrRight:
-        dis.fill(Settings.background)
+        dis.blit(bg_img, (0, 0))
         if last_dir == "right":
             dis.blit(d_idle[0], (xpos, ypos))
         elif last_dir == "left":
             dis.blit(d_idle[1], (xpos, ypos))
-
-    if ypos == 100:
-        m_grounded = True
-    else:
-        m_grounded = False
 
     direction = ""            
     leftOrRight = False
@@ -245,6 +315,31 @@ while run:
     if delay == True:
         pg.event.clear()
         delay = False
+
+    dinoRect = pg.Rect(xpos + 10, ypos + 10, d_width, d_height)
+
+    m_grounded = _check_collisions(dinoRect)
+
+    while m_grounded == False:
+        ypos += 0.1 * (falling_modifier**2)
+        if last_dir == "left":
+            a_num = 3
+        else:
+            a_num = 1
+        falling_modifier += 1
+        dis.blit(bg_img, (0, 0))
+        dis.blit(d_jump[a_num], (xpos, ypos))
+        dinoRect = pg.Rect(xpos + 10, ypos + 10, d_width, d_height)
+        pg.draw.rect(dis, (0,0,255), (0,208,1103,208))
+        pg.draw.rect(dis, (255,0,0), dinoRect)
+        pg.display.flip()
+        pg.time.delay(50)
+        m_grounded = _check_collisions((dinoRect))
+    falling_modifier = 1
+
+    #TESTING
+    pg.draw.rect(dis, (0,0,255), (0,208,1103,208))
+    pg.draw.rect(dis, (255,0,0), dinoRect)
 
     pg.display.flip()                   
 
--- dino_game/settings.py
+++ dino_game/settings.py
@@ -1,5 +1,5 @@
 import os
 class Settings():
     #Display
-    dis_width, dis_height = 600, 600
+    dis_width, dis_height = 1200, 240
     background = (100, 100, 255)
