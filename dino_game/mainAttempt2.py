cb18d1ac2510dee889e3ae37b44d461b08a012da
Umepejo <71135988+Umepejo@users.noreply.github.com>
2021-11-10 13:12:40 +0100

    fucked

--- dino_game/mainAttempt2.py
+++ dino_game/mainAttempt2.py
@@ -6,7 +6,7 @@ from settings import Settings as S
 pg.init()
 cwd = os.getcwd()
 
-def _move_char(flip, x, y, anim_num, m_display, bg, idle):
+def _move_char(flip, x, y, anim_num, m_display, bg, idle, jmp, jump_counter, d_grounded):
     def _image_at(sheet, number, width, height, scale, flip):
             image = pg.Surface((width, height))
             image.blit(sheet, (0, 0), ((number * width), 0, width, height))
@@ -38,9 +38,9 @@ def _move_char(flip, x, y, anim_num, m_display, bg, idle):
 
     #Define
     change = 0
-    speed = 8
+    speed = 4
     char_img = ""
-    s_modif = 1.5
+    s_modif = 1.2
 
     #Animation Lists
     all_animation_lists = []
@@ -75,12 +75,28 @@ def _move_char(flip, x, y, anim_num, m_display, bg, idle):
         change = speed
         x += change
 
+    if jmp:
+        if jump_counter > 0:
+            y -= 1
+            jump_counter -= 1
+    else:
+        y += 1
+
     m_display.blit(char_img, (x, y))
 
     pg.display.flip()
     pg.time.delay(20)
 
     return x, y
+
+def _check_collisions(d_rect, y, x):
+    ground = pg.Rect(0, 208, 1200, 32)
+
+    if pg.Rect.colliderect(d_rect, ground):
+        y += 1
+        return "1", y, x
+    else:
+        return "0"
 
 #Constants
 run = True
@@ -103,10 +119,12 @@ leftOrRight = False
 #Rects
 dinoRect = pg.Rect(ypos + 10, xpos + 10, d_width, d_height)
 #Counters
+jmp_counter = 5
 a_num = 0
 #Lists from functions
 char = []
 jump_return = []
+collisions = []
 
 #Display and background setup
 bg_img = pg.image.load(cwd+'/pygame/dino_game/assets/1-1.png')
@@ -139,19 +157,19 @@ while run == True:
                 run = False
     
     if cur_dir == "right":
-        char = _move_char(False, xpos, ypos, a_num, dis, bg_img, False)
+        char = _move_char(False, xpos, ypos, a_num, dis, bg_img, False, jump, jmp_counter, m_grounded)
         last_dir = "right"
         a_num += 1
     elif cur_dir == "left":
-        char = _move_char(True, xpos, ypos, a_num, dis, bg_img, False)
+        char = _move_char(True, xpos, ypos, a_num, dis, bg_img, False, jump, jmp_counter, m_grounded)
         last_dir = "left"
         a_num += 1
     else:
         a_num = 0
         if last_dir == "right":
-            char = _move_char(False, xpos, ypos, a_num, dis, bg_img, True)
+            char = _move_char(False, xpos, ypos, a_num, dis, bg_img, True, jump, jmp_counter, m_grounded)
         elif last_dir == "left":
-            char = _move_char(True, xpos, ypos, a_num, dis, bg_img, True)
+            char = _move_char(True, xpos, ypos, a_num, dis, bg_img, True, jump, jmp_counter, m_grounded)
    
     if a_num == 3:
         a_num = 0
@@ -160,9 +178,25 @@ while run == True:
     if leftOrRight == True:
         xpos = char[0]
         ypos = char[1]
-        dinoRect = (xpos, ypos, d_width, d_height)
+        dinoRect = pg.Rect(xpos, ypos, d_width, d_height)
+
+    
 
     #Resetting checks
     leftOrRight = False
+
+    jump = False
+
+    collisions = _check_collisions(dinoRect, ypos, xpos)
+    m_grounded = collisions[0]
+    ypos = collisions[1]
+    xpos = collisions[2]
+
+    if m_grounded == "1":
+        m_grounded = True
+    elif m_grounded == "0":
+        m_grounded = False
+    else:
+        print("1")
 
     pg.display.flip()
